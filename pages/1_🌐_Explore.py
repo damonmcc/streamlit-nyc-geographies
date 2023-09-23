d4cabea4db1a68ee294b2b1b1fb0a94499120e92
Damon McCullough <damonmcc1391@gmail.com>
2023-09-23 20:40:28 +0000

    emoji page names

--- pages/1_🌐_Explore.py
+++ pages/1_🌐_Explore.py
@@ -1,13 +1,14 @@
 import streamlit as st
 
 PAGE_HEADER = "Explore Page"
+PAGE_ICON = "🌐"
 
 
 def run_explore():
     st.warning("Please write some code.")
 
 
-st.set_page_config(page_title=PAGE_HEADER, page_icon="🌐")
+st.set_page_config(page_title=PAGE_HEADER, page_icon=PAGE_ICON)
 st.markdown(f"# {PAGE_HEADER}")
 st.sidebar.header(PAGE_HEADER)
 
