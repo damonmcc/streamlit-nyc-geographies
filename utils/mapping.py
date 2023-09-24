c0e6906bf6f63c7caf3d54768eb29802adc1f81f
Damon McCullough <damonmcc1391@gmail.com>
2023-09-24 01:50:52 +0000

    format and stuff

--- utils/mapping.py
+++ utils/mapping.py
@@ -4,15 +4,18 @@ import leafmap.foliumap as leafmap
 NYC_CENTER_COORDS = [40.726446, -73.983307]
 NYC_CENTER_ZOOM = 12
 
+
 def show_basic_map(geodata: gpd.GeoDataFrame, name: str):
-    m = leafmap.Map(tiles='stamenterrain', center=NYC_CENTER_COORDS, zoom=NYC_CENTER_ZOOM)
+    m = leafmap.Map(
+        tiles="stamenterrain", center=NYC_CENTER_COORDS, zoom=NYC_CENTER_ZOOM
+    )
     m.add_gdf(geodata, layer_name=name)
     m.to_streamlit(width=600, height=700)
 
 
 # @st.cache_data(ttl=120)
 def show_demo_map():
-    m = leafmap.Map(tiles='stamenterrain', center=NYC_CENTER_COORDS, zoom=12)
+    m = leafmap.Map(tiles="stamenterrain", center=NYC_CENTER_COORDS, zoom=12)
     # url = "https://github.com/opengeos/leafmap/blob/master/examples/data/countries.geojson"
     # m.add_vector(url, fill_alpha=0.5, fill_color="lightblue")
     # cable_lines_geojson = 'https://raw.githubusercontent.com/opengeos/leafmap/master/examples/data/cable_geo.geojson'
